bf4d57ddecf2ee26cb13f8c548594324b9583d39
杨川 <471448446@qq.com>
2020-04-07 17:21:44 +0800

    doc: mark string

--- CProgramingLanguage/var_string.c
+++ CProgramingLanguage/var_string.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
+//https://www.runoob.com/cprogramming/c-strings.html
 int main(int argc, char const *argv[])
 {
 	/**
-	* 申明字符串 指定 char值的时候，必须加\0
+	* 申明字符串 指定 char值的时候，必须加 \0
+	* 字符串在存储的时候回多一个空字符，长度是+1的
 	*/
     char hello2[] = {'h','e','l','l','o','\0'};
 	char hello1[] = "hello";
