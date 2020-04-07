557d3b7110a68de82402cc8471fa27508f1964f6
杨川 <471448446@qq.com>
2020-04-07 21:58:39 +0800

    feature: string chinese len

--- CProgramingLanguage/var_string.c
+++ CProgramingLanguage/var_string.c
@@ -8,10 +8,13 @@ int main(int argc, char const *argv[])
 	* 申明字符串 指定 char值的时候，必须加 \0
 	* 字符串在存储的时候回多一个空字符，长度是+1的
 	*/
-    char hello2[] = {'h','e','l','l','o','\0'};
 	char hello1[] = "hello";
-    printf("%s size = %d \n", hello1,strlen(hello1));
-    printf("%s size = %d \n", hello2,strlen(hello2));
+    char hello2[] = {'h','e','l','l','o','\0'};
+	char name_chinese[] = "张";
+    printf("%s size = %d \n", hello1,(int)strlen(hello1));
+    printf("%s size = %d \n", hello2,(int)strlen(hello2));
+    printf("%s size = %d \n", name_chinese,(int)strlen(name_chinese));
+
 	/* code */
 	return 0;
 }
