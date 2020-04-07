d9be9e204032d16284e200d073cdb4e3a0bf15d6
杨川 <471448446@qq.com>
2020-04-07 22:14:17 +0800

    feature: string

--- CProgramingLanguage/var_string.c
+++ CProgramingLanguage/var_string.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+* 直接赋值和使用strcopy区别？？
+*/
 //https://www.runoob.com/cprogramming/c-strings.html
 int main(int argc, char const *argv[])
 {
@@ -14,6 +16,22 @@ int main(int argc, char const *argv[])
     printf("%s size = %d \n", hello1,(int)strlen(hello1));
     printf("%s size = %d \n", hello2,(int)strlen(hello2));
     printf("%s size = %d \n", name_chinese,(int)strlen(name_chinese));
+
+    // 申明长度是12 实际上只有5
+    char str1[12] = "Hello";
+    char str2[12] = "World";
+    // 我只是申明 字符串 但是必须指定长度
+    char str3[12];
+
+    /* 复制 str1 到 str3 */
+    strcpy(str3,str1);
+    printf("strcpy( str3, str1) :  %s \n", str3);
+    /* 连接 str1 和 str2 */
+    strcat( str1, str2);
+    printf("strcat( str1, str2):   %s\n", str1 );
+       /* 连接后，str1 的总长度 */
+    int len = strlen(str1);
+    printf("strlen(str1) :  %d\n", len );
 
 	/* code */
 	return 0;
