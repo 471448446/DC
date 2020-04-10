b27db6565fcac24e11a8faf236734c923670de76
杨川 <471448446@qq.com>
2020-04-10 21:04:05 +0800

    feature(point): array

--- CProgramingLanguage/pointer_++.c
+++ CProgramingLanguage/pointer_++.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 
+//https://stackoverflow.com/questions/8208021/how-to-increment-a-pointer-address-and-pointers-value
 int main(int argc, char const *argv[])
 {
 	int array[] = {10,20,200};
 	int i,*point;
 	/**
 	* 指针赋值，指向数组中第一个位置的内存地址。
+	* point+1 指向下一个地址 
 	*/
 	point = array;
+	printf("------use point++ -----\n");
 	for (i = 0; i < 3 ; i++)
 	{
-		// 这里有个类型转换的警告⚠️
-		printf("address: array[%d] = %x\n",i,(unsigned int)point );
-		printf("value: array[%d] = %d\n",i,*point );
+		// 这里有个类型转换的警告⚠️  %x
+		printf("array[%d] = %d , %p\n",i,*point,point );
 		point++;
+	}
+	printf("------use point+1 -----\n");
+	point = &array[0];
+	for (int i = 0; i < 3; ++i)
+	{
+		printf("point[%d] = %d , %p\n", i,point[i],point+i);
 	}
 	return 0;
 }
